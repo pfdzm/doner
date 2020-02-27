821a44b9ae1d901e9e61ee613982e7ff62311958
Pablo Fernandez <pfdzm@users.noreply.github.com>
2020-02-27 21:26:16 +0100

    Define carousel component to flip cards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,12 @@ import React from "react";
 
 import Button from "../components/Button";
 
-import Card from "../components/Card";
+import Carousel from "../components/Carousel";
 
 export default function Home() {
   return (
     <div className="container mx-auto w-4/5 lg:px-4 py-5">
-      <Card></Card>
+      <Carousel />
       <Button withLogo linkTo="donate">
         Dönate
       </Button>
