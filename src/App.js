0dff496df6847095ab95e63698ca3a384f296573
Pablo Fernandez <pfdzm@users.noreply.github.com>
2020-03-01 22:26:16 +0100

    add css transitions to nav/router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 import { DonateProvider } from "./utils/GlobalState";
 
@@ -15,14 +16,33 @@ function App() {
   return (
     <div className="bg-gray-700 min-h-screen">
       <Router>
-        <DonateProvider>
-          <Navigation />
-          <Switch>
-            <Route exact path="/" component={Home}></Route>
-            <Route exact path="/stats" component={Stats}></Route>
-            <Route exact path="/donate" component={Donate}></Route>
-          </Switch>
-        </DonateProvider>
+        <Route
+          render={({ location }) => (
+            <DonateProvider>
+              <Navigation />
+              <TransitionGroup>
+                <CSSTransition
+                  key={location.key}
+                  classNames="scale"
+                  timeout={300}
+                >
+                  <Switch location={location}>
+                    <Route exact path="/" component={Home}></Route>
+                    <Route exact path="/stats" component={Stats}></Route>
+                    <Route exact path="/donate" component={Donate}></Route>
+                    <Route
+                      render={() => (
+                        <div className="p-5 w-11/12 mx-auto rounded border shadow bg-gray-200 m-3 text-xl text-gray-700">
+                          Oops ... Not Found
+                        </div>
+                      )}
+                    />
+                  </Switch>
+                </CSSTransition>
+              </TransitionGroup>
+            </DonateProvider>
+          )}
+        />
       </Router>
     </div>
   );
