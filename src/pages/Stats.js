aaa90a10ec6e031c9d0f13f106e03f5978a7da0d
Pablo Fernandez <pfdzm@users.noreply.github.com>
2020-03-01 22:38:51 +0100

    adjust max-w on desktop stats

--- src/pages/Stats.js
+++ src/pages/Stats.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Stats() {
   return (
-    <div className="p-5 w-11/12 mx-auto rounded border shadow bg-gray-200 m-3 text-xl text-gray-700">
+    <div className="p-5 w-11/12 max-w-xl mx-auto rounded border shadow bg-gray-200 m-3 text-xl text-gray-700">
       <h1 className="p-5 bg-yellow-300 rounded shadow border">
         Thank you, donors!
       </h1>
