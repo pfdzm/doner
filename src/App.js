a83560ed27bd5c9819e4697c1e4585c1027dd481
Pablo Fernandez <pfdzm@users.noreply.github.com>
2020-03-05 21:30:45 +0100

    fix some styling, add rand data to chart

--- src/App.js
+++ src/App.js
@@ -14,37 +14,31 @@ import "./styles/App.css";
 
 function App() {
   return (
-    <div className="bg-gray-700 min-h-screen">
-      <Router>
-        <Route
-          render={({ location }) => (
-            <DonateProvider>
-              <Navigation />
-              <TransitionGroup>
-                <CSSTransition
-                  key={location.key}
-                  classNames="fade"
-                  timeout={300}
-                >
-                  <Switch location={location}>
-                    <Route exact path="/" component={Home}></Route>
-                    <Route exact path="/stats" component={Stats}></Route>
-                    <Route exact path="/donate" component={Donate}></Route>
-                    <Route
-                      render={() => (
-                        <div className="p-5 w-11/12 mx-auto rounded border shadow bg-gray-200 m-3 text-xl text-gray-700">
-                          Oops ... Not Found
-                        </div>
-                      )}
-                    />
-                  </Switch>
-                </CSSTransition>
-              </TransitionGroup>
-            </DonateProvider>
-          )}
-        />
-      </Router>
-    </div>
+    <Router>
+      <Route
+        render={({ location }) => (
+          <DonateProvider>
+            <Navigation />
+            <TransitionGroup>
+              <CSSTransition key={location.key} classNames="fade" timeout={300}>
+                <Switch location={location}>
+                  <Route exact path="/" component={Home}></Route>
+                  <Route exact path="/stats" component={Stats}></Route>
+                  <Route exact path="/donate" component={Donate}></Route>
+                  <Route
+                    render={() => (
+                      <div className="p-5 w-11/12 mx-auto rounded border shadow bg-gray-200 mt-3 text-xl text-gray-700">
+                        Oops ... Not Found
+                      </div>
+                    )}
+                  />
+                </Switch>
+              </CSSTransition>
+            </TransitionGroup>
+          </DonateProvider>
+        )}
+      />
+    </Router>
   );
 }
 
--- src/pages/Stats.js
+++ src/pages/Stats.js
@@ -1,46 +1,67 @@
 import React, { createRef, useEffect } from "react";
 import Chart from "chart.js";
+import fetchCharities from "../utils/API";
 
 const canvasRef = createRef();
 export default function Stats() {
   useEffect(() => {
     const ctx = canvasRef.current.getContext("2d");
-    const chart = new Chart(ctx, {
-      // The type of chart we want to create
-      type: "line",
 
-      // The data for our dataset
-      data: {
-        labels: [
-          "January",
-          "February",
-          "March",
-          "April",
-          "May",
-          "June",
-          "July"
-        ],
-        datasets: [
-          {
-            label: "My First dataset",
-            backgroundColor: "rgb(255, 99, 132)",
-            borderColor: "rgb(255, 99, 132)",
-            data: [0, 10, 5, 2, 20, 30, 45]
-          }
-        ]
-      },
+    fetchCharities().then(data => {
+      const charityLabels = data.map(charity => charity.name);
 
-      // Configuration options go here
-      options: {}
+      const chart = new Chart(ctx, {
+        // The type of chart we want to create
+        type: "line",
+
+        // The data for our dataset
+        data: {
+          labels: [
+            "January",
+            "February",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July"
+          ],
+          datasets: [
+            ...charityLabels.map(name => ({
+              label: name,
+              backgroundColor: "",
+              borderColor: "",
+              data: [
+                Math.floor(Math.random() * 255),
+                Math.floor(Math.random() * 255),
+                Math.floor(Math.random() * 255),
+                Math.floor(Math.random() * 255),
+                Math.floor(Math.random() * 255),
+                Math.floor(Math.random() * 255),
+                Math.floor(Math.random() * 255)
+              ]
+            }))
+          ]
+        },
+
+        // Configuration options go here
+        options: {}
+      });
     });
   }, []);
+
   return (
     <div className="p-5 w-11/12 max-w-xl mx-auto rounded border shadow bg-gray-200 m-3 text-xl text-gray-700">
       <h1 className="py-5 font-display text-2xl">Thank you, donors!</h1>
       <p className="leading-7">
         With your help, we are helping those Berliners that need it most.
       </p>
-      <canvas id="myChart" width="400" height="400" ref={canvasRef}></canvas>
+      <canvas
+        id="myChart"
+        className="mb-5"
+        width="400"
+        height="400"
+        ref={canvasRef}
+      ></canvas>
     </div>
   );
 }
