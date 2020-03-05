92c39dedb1e82acdda2394d86221e1e2587c0600
Pablo Fernandez <pfdzm@users.noreply.github.com>
2020-03-05 15:00:06 +0100

    use chart.js for stats page

--- src/pages/Stats.js
+++ src/pages/Stats.js
@@ -1,16 +1,45 @@
-import React from "react";
+import React, { createRef, useEffect } from "react";
+import Chart from "chart.js";
 
+const canvasRef = createRef();
 export default function Stats() {
+  useEffect(() => {
+    const ctx = canvasRef.current.getContext("2d");
+    const chart = new Chart(ctx, {
+      // The type of chart we want to create
+      type: "line",
+
+      // The data for our dataset
+      data: {
+        labels: [
+          "January",
+          "February",
+          "March",
+          "April",
+          "May",
+          "June",
+          "July"
+        ],
+        datasets: [
+          {
+            label: "My First dataset",
+            backgroundColor: "rgb(255, 99, 132)",
+            borderColor: "rgb(255, 99, 132)",
+            data: [0, 10, 5, 2, 20, 30, 45]
+          }
+        ]
+      },
+
+      // Configuration options go here
+      options: {}
+    });
+  }, []);
   return (
     <div className="p-5 w-11/12 max-w-xl mx-auto rounded border shadow bg-gray-200 m-3 text-xl text-gray-700">
       <h1 className="p-5 bg-yellow-300 rounded shadow border">
         Thank you, donors!
       </h1>
-      <ul className="p-5 my-3 leading-normal">
-        <li>Peter, 3.50</li>
-        <li>Wiebke, 3.50</li>
-        <li>Friedrich, 3.50</li>
-      </ul>
+      <canvas id="myChart" width="400" height="400" ref={canvasRef}></canvas>
     </div>
   );
 }
