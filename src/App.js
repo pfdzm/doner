d0226f05ee21875f96bba608ddb03e9b5fd04eac
Pablo Fernandez <pfdzm@users.noreply.github.com>
2020-03-06 09:10:46 +0100

    build a 404 page/component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { DonateProvider } from "./utils/GlobalState";
 import Donate from "./pages/Donate";
 import Home from "./pages/Home";
 import Stats from "./pages/Stats";
+import NotFound from "./pages/NotFound";
 
 import Navigation from "./components/Navigation";
 
@@ -25,13 +26,7 @@ function App() {
                   <Route exact path="/" component={Home}></Route>
                   <Route exact path="/stats" component={Stats}></Route>
                   <Route exact path="/donate" component={Donate}></Route>
-                  <Route
-                    render={() => (
-                      <div className="p-5 w-11/12 mx-auto rounded border shadow bg-gray-200 mt-3 text-xl text-gray-700">
-                        Oops ... Not Found
-                      </div>
-                    )}
-                  />
+                  <Route render={() => <NotFound />} />
                 </Switch>
               </CSSTransition>
             </TransitionGroup>
