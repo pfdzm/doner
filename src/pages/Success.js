92fecda1adbd357f0ca1b8216d551ae3a3bc7330
Pablo Fernandez <pfdzm@users.noreply.github.com>
2020-03-06 22:54:38 +0100

    add stats button to success page

--- src/pages/Success.js
+++ src/pages/Success.js
@@ -1,12 +1,14 @@
 import React from "react";
+import StatsButton from "../components/StatsButton";
 
-const Success = () => {
-  return (
-    <div className="p-5 w-11/12 max-w-xl mx-auto rounded border shadow bg-gray-200 my-5 text-xl text-gray-700">
+const Success = () => (
+  <div className="w-11/12 mx-auto">
+    <div className="p-5 max-w-xl mx-auto rounded border shadow bg-gray-200 my-5 text-xl text-gray-700">
       <h1 className="py-5 font-display text-2xl">Thank you!</h1>
       <p className="leading-7">Your donation will be put to good use!</p>
     </div>
-  );
-};
+    <StatsButton />
+  </div>
+);
 
 export default Success;
