bbbea1d0300e5509286fea4af10b939dfc4d36b5
Pablo Fernandez <pablo.fernandez-michel@enpal.de>
2020-11-28 21:37:21 +0100

    Split code a bit more

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,16 @@
-import React from "react";
-import CharityInfoBox from "./CharityInfoBox";
+import React, { lazy, Suspense } from "react";
 import Loading from "./Loading";
 
+const CharityInfoBox = lazy(() => import("./CharityInfoBox"));
+
 const Card = ({ loading, charity }) => {
-  return loading ? <Loading /> : <CharityInfoBox charity={charity} />;
+  return loading ? (
+    <Loading />
+  ) : (
+    <Suspense>
+      <CharityInfoBox charity={charity} />
+    </Suspense>
+  );
 };
 
 export default Card;
