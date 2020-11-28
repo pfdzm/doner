82e0a33d9acd2334fdca91979434c2784cd9a202
Pablo Fernandez <pablo.fernandez-michel@enpal.de>
2020-11-28 23:16:24 +0100

    Lazy load chartjs

--- src/pages/Stats.js
+++ src/pages/Stats.js
@@ -1,5 +1,5 @@
 import React, { createRef, useEffect, useState } from "react";
-import Chart from "chart.js";
+// import Chart from "chart.js";
 import fetchCharities from "../utils/API";
 import Loading from "../components/Loading";
 import Layout from "../components/Layout";
@@ -11,61 +11,63 @@ export default function Stats() {
   useEffect(() => {
     const ctx = canvasRef.current.getContext("2d");
 
-    fetchCharities().then(data => {
-      const charityLabels = data.map(charity => charity.name);
+    fetchCharities().then((data) => {
+      const charityLabels = data.map((charity) => charity.name);
 
       // eslint-disable-next-line
-      const chart = new Chart(ctx, {
-        // The type of chart we want to create
-        type: "bar",
+      import("chart.js").then((chart) => {
+        new chart.Chart(ctx, {
+          // The type of chart we want to create
+          type: "bar",
 
-        // The data for our dataset
-        data: {
-          labels: [
-            "January",
-            "February",
-            "March",
-            "April",
-            "May",
-            "June",
-            "July"
-          ],
-          datasets: [
-            ...charityLabels.map(name => {
-              const r = Math.floor(Math.random() * 255);
-              const g = Math.floor(Math.random() * 255);
-              const b = Math.floor(Math.random() * 255);
+          // The data for our dataset
+          data: {
+            labels: [
+              "January",
+              "February",
+              "March",
+              "April",
+              "May",
+              "June",
+              "July",
+            ],
+            datasets: [
+              ...charityLabels.map((name) => {
+                const r = Math.floor(Math.random() * 255);
+                const g = Math.floor(Math.random() * 255);
+                const b = Math.floor(Math.random() * 255);
 
-              return {
-                label: name,
-                backgroundColor: `rgb(${r}, ${g}, ${b})`,
-                borderColor: `rgb(${r}, ${g}, ${b})`,
-                data: [
-                  Math.floor(Math.random() * 500),
-                  Math.floor(Math.random() * 500),
-                  Math.floor(Math.random() * 500),
-                  Math.floor(Math.random() * 500),
-                  Math.floor(Math.random() * 500),
-                  Math.floor(Math.random() * 500),
-                  Math.floor(Math.random() * 500)
-                ]
-              };
-            })
-          ]
-        },
-
-        // Configuration options go here
-        options: {
-          scales: {
-            yAxes: [{ stacked: true }],
-            xAxes: [{ stacked: true }]
+                return {
+                  label: name,
+                  backgroundColor: `rgb(${r}, ${g}, ${b})`,
+                  borderColor: `rgb(${r}, ${g}, ${b})`,
+                  data: [
+                    Math.floor(Math.random() * 500),
+                    Math.floor(Math.random() * 500),
+                    Math.floor(Math.random() * 500),
+                    Math.floor(Math.random() * 500),
+                    Math.floor(Math.random() * 500),
+                    Math.floor(Math.random() * 500),
+                    Math.floor(Math.random() * 500),
+                  ],
+                };
+              }),
+            ],
           },
-          animation: {
-            onProgress: () => {
-              setLoading(false);
-            }
-          }
-        }
+
+          // Configuration options go here
+          options: {
+            scales: {
+              yAxes: [{ stacked: true }],
+              xAxes: [{ stacked: true }],
+            },
+            animation: {
+              onProgress: () => {
+                setLoading(false);
+              },
+            },
+          },
+        });
       });
     });
   }, []);
