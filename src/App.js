6d881bda679c6cee01d58b4292a3f3d35260449b
Pablo Fernandez <pablo.fernandez-michel@enpal.de>
2020-11-28 20:58:44 +0100

    Lazy load routes

--- src/App.js
+++ src/App.js
@@ -1,42 +1,46 @@
-import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { TransitionGroup, CSSTransition } from "react-transition-group";
-
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import "./styles/App.css";
 import { DonateProvider } from "./utils/GlobalState";
 
-import Donate from "./pages/Donate";
-import Home from "./pages/Home";
-import Stats from "./pages/Stats";
-import NotFound from "./pages/NotFound";
-import Success from "./pages/Success";
-import Canceled from "./pages/Canceled";
+const Donate = lazy(() => import("./pages/Donate"));
+const Home = lazy(() => import("./pages/Home"));
+const Stats = lazy(() => import("./pages/Stats"));
+const NotFound = lazy(() => import("./pages/NotFound"));
+const Success = lazy(() => import("./pages/Success"));
+const Canceled = lazy(() => import("./pages/Canceled"));
+const Navigation = lazy(() => import("./components/Navigation"));
 
-import Navigation from "./components/Navigation";
-
-import "./styles/App.css";
 
 function App() {
   return (
     <Router>
-      <Route
-        render={({ location }) => (
-          <DonateProvider>
-            <Navigation />
-            <TransitionGroup>
-              <CSSTransition key={location.key} classNames="fade" timeout={300}>
-                <Switch location={location}>
-                  <Route exact path="/" component={Home}></Route>
-                  <Route exact path="/stats" component={Stats}></Route>
-                  <Route exact path="/donate" component={Donate}></Route>
-                  <Route exact path="/success" component={Success}></Route>
-                  <Route exact path="/canceled" component={Canceled}></Route>
-                  <Route render={() => <NotFound />} />
-                </Switch>
-              </CSSTransition>
-            </TransitionGroup>
-          </DonateProvider>
-        )}
-      />
+      <Suspense fallback={<div>Loading...</div>}>
+        <Route
+          render={({ location }) => (
+            <DonateProvider>
+              <Navigation />
+              <TransitionGroup>
+                <CSSTransition
+                  key={location.key}
+                  classNames="fade"
+                  timeout={300}
+                >
+                  <Switch location={location}>
+                    <Route exact path="/" component={Home}></Route>
+                    <Route exact path="/stats" component={Stats}></Route>
+                    <Route exact path="/donate" component={Donate}></Route>
+                    <Route exact path="/success" component={Success}></Route>
+                    <Route exact path="/canceled" component={Canceled}></Route>
+                    <Route render={() => <NotFound />} />
+                  </Switch>
+                </CSSTransition>
+              </TransitionGroup>
+            </DonateProvider>
+          )}
+        />
+      </Suspense>
     </Router>
   );
 }
